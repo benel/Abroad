ade6cc7bbd7177d615e9ab4da0337c1d4b6bb1bb
loicmoinet <loic.moinet@utt.fr>
2014-06-05 15:39:07 +0200

    Update issue_21.rb

--- spec/features/issue_21.rb
+++ spec/features/issue_21.rb
@@ -1,43 +1,37 @@
 require 'spec_helper.rb'
 
-feature 'Edit module' do
+feature 'Set course equivalence' do
   
-  # Cas ou le responsable de fillière accède à la page d'édition de module via un l'e-mail
   scenario 'from email' do
     visit'/'
     click_on 'Editer le module'
     fill_in 'Equivalence', :with => 'ISI/MPL/CS/IF05'
-    fill_in 'Commentaires', :with => 'Ce cours na pas encore été suivi par un étudiant de lUTT une fois sur place.'
+    fill_in 'Commentaires', :with => 'Ce cours traite partiellement du contenu d IF05 mais le sujet des méthodes agiles n est pas abordé.'
     click_on 'Enregistrer'
     bookmark = current_url
     visit '/'
-    #page.should_not have_content 'Supercolor-Tryphonar'
     visit bookmark
     page.should have_field 'Equivalence', :with => 'ISI/MPL/CS/IF05'
-    page.should have_field 'Commentaires', :with => 'Ce cours na pas encore été suivi par un étudiant de lUTT une fois sur place.'
+    page.should have_field 'Commentaires', :with => 'Ce cours traite partiellement du contenu d IF05 mais le sujet des méthodes agiles n est pas abordé.'
   
   end
-  
-  
-  # Cas ou le responsable de fillière accède à la page en navigant via l'application
+
   scenario 'from home page' do
     visit'/'
     click_on 'Liste des étudiants'
     click_on 'Jean DUPONT'
     click_on 'Editer le module'
     fill_in 'Equivalence', :with => 'ISI/MPL/CS/IF05'
-    fill_in 'Commentaires', :with => 'Ce cours na pas encore été suivi par un étudiant de lUTT une fois sur place.'
+    fill_in 'Commentaires', :with => 'Ce cours traite partiellement du contenu d IF05 mais le sujet des méthodes agiles n est pas abordé.'
     click_on 'Enregistrer'
     bookmark = current_url
     visit '/'
     visit bookmark
     page.should have_field   'Equivalence', :with => 'ISI/MPL/CS/IF05'
-    page.should have_content 'Ce cours na pas encore été suivi par un étudiant de lUTT une fois sur place.'
+    page.should have_content 'Ce cours traite partiellement du contenu d IF05 mais le sujet des méthodes agiles n est pas abordé.'
   
   end
   
-  
-  # Cas ou on laisse le champ Code vide
   scenario 'with empty Code field' do
     visit '/profile/Jean-DUPONT'
     click_on 'Editer le module'

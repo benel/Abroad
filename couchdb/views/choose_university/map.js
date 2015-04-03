b5c256d2ba54cbfb935bc1c8bc5b1022386a2bc4
Aurelien Benel <aurelien.benel@utt.fr>
2015-04-03 12:20:16 +0200

    FEATURE: Select curriculum on the home page.

--- couchdb/views/choose_university/map.js
+++ couchdb/views/choose_university/map.js
@@ -3,6 +3,7 @@ function(o) {
     var e = c.equivalence;
     if (e && e!="CT" && e!="ME" && e!="EC") {
       emit([e, o.host_institution]);
+      emit([':curriculum', o.host_institution]); //Instead of '', workaround for CouchDB bug 
     }
   }
 }
